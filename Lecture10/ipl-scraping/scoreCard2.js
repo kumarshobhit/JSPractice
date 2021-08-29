a838ed898fec91dcfea6e59f43f7afc7639d428d
unknown <ishobhit3@gmail.com>
2021-08-29 09:24:14 +0530

    ipl act comp

--- Lecture10/ipl-scraping/scoreCard2.js
+++ Lecture10/ipl-scraping/scoreCard2.js
@@ -28,7 +28,6 @@ function dataExtracter(html) {
     let bothInningArr = searchTool(".Collapsible");
     let data=searchTool('.match-info.match-info-MATCH.match-info-MATCH-half-width .description')
     data=data.text().trim().split(',');
-    let matchNo=data[0]
     let venue=data[1] ;
     let date=data[2] ;
 
@@ -36,34 +35,36 @@ function dataExtracter(html) {
         // team name
         let teamName=searchTool(bothInningArr[i]).find('h5').text().trim() ;
         teamName=teamName.split('INNINGS')[0] ;
+        teamName = teamName.trim();
         // opponent team
         let opponentIndex= i == 0 ? 1:0;
         let opponentName=searchTool(bothInningArr[opponentIndex]).find('h5').text().trim() ;
         opponentName=opponentName.split('INNINGS')[0];
+        opponentName = opponentName.trim();
 
         let batsmanArr=searchTool(bothInningArr[i]).find('.table.batsman tbody tr')
         for(let j=0;j<batsmanArr.length;j++) {
             let cols=searchTool(batsmanArr[j]).find("td") ;
             if(cols.length !== 8) continue ;
-             let name=searchTool(cols[0]).text().trim() ;
+             let playerName = searchTool(cols[0]).text();
              let runs=searchTool(cols[2]).text().trim() ;
              let balls=searchTool(cols[3]).text().trim() ;
              let fours=searchTool(cols[5]).text().trim() ;
              let sixes=searchTool(cols[6]).text().trim() ;
              let sr=searchTool(cols[7]).text().trim() ;
-            processPlayer(teamName,name,runs,balls,fours,sixes,sr,opponentName,date,venue);
+            processPlayer(teamName,playerName,runs,balls,fours,sixes,sr,opponentName,date,venue);
         }
         console.log("***********************************")
     }
 }
 
-function processPlayer(teamName,name,runs,balls,fours,sixes,sr,opponentName,date,venue) {
-    name=name.split(' ');
-    let playerName=name[0]+name[1] ;
+function processPlayer(teamName,playerName,runs,balls,fours,sixes,sr,opponentName,date,venue) {
+    
     let teamPath=path.join(__dirname,"ipl",teamName);
     dirCreater(teamPath);
     let filePath=path.join(teamPath,playerName+".xlsx");
-    let content=excelReader(filePath,playerName);
+    let content=[] ;
+   
     let playerObj={
         teamName,
         playerName,
@@ -76,7 +77,13 @@ function processPlayer(teamName,name,runs,balls,fours,sixes,sr,opponentName,date
         date,
         venue
     }
-    content.push(playerObj);
+     if(fs.existsSync(filePath)==false) {
+        content.push(playerObj)
+    }
+    else {
+         content = excelReader(filePath, playerName);
+        content.push(playerObj);
+    }
     excelWriter(filePath,content,playerName);
 }
 
@@ -87,6 +94,7 @@ function dirCreater(filePath) {
 }
 
 function excelWriter(filePath,json,sheetName) {
+    // console.log(json) ;
     let newWb=xlsx.utils.book_new() ;
     let newWs=xlsx.utils.json_to_sheet(json) ;
     xlsx.utils.book_append_sheet(newWb,newWs,sheetName);
@@ -95,9 +103,7 @@ function excelWriter(filePath,json,sheetName) {
 }
 
 function excelReader(filePath,sheetName) {
-    if(fs.existsSync(filePath) == false) {
-        return [] ;
-    }
+    
     let wb=xlsx.readFile(filePath);
     let excelData=wb.Sheets[sheetName];
     let ans=xlsx.utils.sheet_to_json(excelData);
--- Lecture11/n-files/async-serial.js
+++ Lecture11/n-files/async-serial.js
@@ -2,12 +2,14 @@ let fs=require('fs');
 let files=["f1.txt", "f2.txt", "f3.txt", "f4.txt", "f5.txt"] ;
 console.log("before") ;
 
-for(let i=0;i<files.length;) {
-    console.log("i",i);
-    fs.readFile(files[i],function cb(err,data){
-        console.log("data"+data);
-        i++ ;
-    });
-}
+// for(let i=0;i<files.length;) {
+//     console.log("i",i);
+//     fs.readFile(files[i],function cb(err,data){
+//         console.log("data"+data);
+//         i++ ;
+//     });
+// }
+
+
 console.log("after");
 
