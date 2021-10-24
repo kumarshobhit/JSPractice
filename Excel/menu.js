8d503f6cada2c2f7155646d43b970dbd4b4c7c3a
unknown <ishobhit3@gmail.com>
2021-10-24 10:17:12 +0530

    added till change font size

--- Excel/menu.js
+++ Excel/menu.js
@@ -1,7 +1,12 @@
 let fontSizeInput=document.querySelector('.font_size_input') ;
 
-fonSizeInput.addEventListener('change',e=>{
-
+fontSizeInput.addEventListener('change',e=>{
+    let fontSize=fontSizeInput.value ;
+    let address=addressInput.value ;
+    let ridcidObj=getRidCidFromAddress(address) ;
+    let toBeChangedCell=document.querySelector(`.grid .cell[rId='${ridcidObj.rid}'][cId='${ridcidObj.cid}']`) ;
+    // change fontSize property 
+    toBeChangedCell.style.fontSize=fontSize+"px" ;
 })
 
 // select -> font family
